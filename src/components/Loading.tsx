35452cae7f3f9edba8f38b0f5452302161aebf52
Sam Jones <sam@refractar.com>
2024-06-25 15:18:19 -0600

    kkk

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -12,22 +12,35 @@ export const Loading = ({ onStartButtonClick, camKitLoaded }: Props) => {
           <li>
             <span>1</span>
             <img src="magnifying-glass.png" />
-            <p>enable motion permission</p>
+            <p>Find a flat surface and scan the floor</p>
           </li>
 
           <li>
             <span>2</span>
             <img src="camera.png" />
-            <p>Aim your phone camera at the correct images</p>
+            <p>Aim your camera at the car</p>
           </li>
           <li>
             <span>3</span>
-            <img src="tap.png" />
-            <p>Solve the puzzel!</p>
+            <img src="power.png" />
+            <p>Tap to collect</p>
+          </li>
+
+          <li>
+            <span>4</span>
+            <img src="repeat.png" />
+            <p>Win rewards!</p>
           </li>
         </ol>
         <br />
-
+        <p>
+          If you qualify for a half-court shot and successfully complete it,
+          you’ll win::
+        </p>
+        <article>
+          <img src="truck.png" />
+          Lynford Ford
+        </article>
         <button
           disabled={!camKitLoaded}
           onClick={() => {
